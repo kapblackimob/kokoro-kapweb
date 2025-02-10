8defd18f1b9a69567ea220ee887af65a7948a299
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-10 00:51:19 +0000

    Refactor phonemization process to use phonemizer CLI and improve text normalization

--- src/lib/server/phonemizer/index.ts
+++ src/lib/server/phonemizer/index.ts
@@ -5,7 +5,7 @@ const exec = util.promisify(child_process.exec);
 // https://github.com/espeak-ng/espeak-ng/blob/master/docs/languages.md
 const espeakLangs = {
   us_english: "en-us",
-  eu_english: "en",
+  eu_english: "en-gb",
   la_spanish: "es-419",
   eu_spanish: "es",
 };
@@ -26,8 +26,13 @@ const langs = {
 export type Lang = keyof typeof langs;
 
 /**
- * phonemize converts text to phonemes using espeak-ng and returns
+ * phonemize converts text to phonemes and returns
  * the phonemized text in the specified language.
+ *
+ * it uses python phonemizer and espeak-ng backend
+ * to generate the phonemes.
+ *
+ * https://bootphon.github.io/phonemizer/cli.html
  *
  * @param text
  * @param lang
@@ -35,19 +40,51 @@ export type Lang = keyof typeof langs;
  */
 export async function phonemize(text: string, lang: Lang): Promise<string> {
   const langCode = langs[lang] ?? espeakLangs.us_english;
-
+  text = normalizeText(text);
   text = text.replaceAll('"', '\\"');
-  const cmdParts = [
-    "espeak-ng",
-    "-q",
-    "--ipa",
-    "-v",
-    langCode,
-    `"${text}"`,
-  ];
 
-  const { stdout, stderr } = await exec(cmdParts.join(" "));
+  const cmd = `
+    echo "${text}" | \
+    /usr/local/bin/phonemize \
+    --quiet \
+    --preserve-punctuation \
+    --with-stress \
+    --backend espeak \
+    --language ${langCode}
+  `;
+
+  const { stdout, stderr } = await exec(cmd);
   if (stderr) throw new Error(stderr);
 
   return stdout.split("\n").join(" ").trim();
+}
+
+/**
+ * normalizeText normalizes text to be phonemized.
+ *
+ * @param text The text to normalize.
+ */
+function normalizeText(text: string): string {
+  return (
+    text
+      // Quotes and parentheses
+      .replaceAll("‘", "'")
+      .replaceAll("’", "'")
+      .replaceAll("«", "(")
+      .replaceAll("»", ")")
+      .replaceAll("“", '"')
+      .replaceAll("”", '"')
+      // Punctuation
+      .replace(/、/g, ", ")
+      .replace(/。/g, ". ")
+      .replace(/！/g, "! ")
+      .replace(/，/g, ", ")
+      .replace(/：/g, ": ")
+      .replace(/；/g, "; ")
+      .replace(/？/g, "? ")
+      // Spaces
+      .replaceAll("\n", "  ")
+      .replaceAll("\t", "  ")
+      .trim()
+  );
 }

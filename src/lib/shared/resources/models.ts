7976c27a640723b7089ac83b497dedc733272316
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-10 16:33:47 +0000

    Refactor model identifiers to use modelId for consistency and improve type definitions

--- src/lib/shared/resources/models.ts
+++ src/lib/shared/resources/models.ts
@@ -1,44 +1,44 @@
 export type Model = (typeof models)[number];
-export type ModelId = Model["model"];
+export type ModelId = Model["modelId"];
 
 export const models = Object.freeze([
   {
-    model: "model",
+    modelId: "model",
     quantization: "fp32",
     size: "326 MB",
   },
   {
-    model: "model_fp16",
+    modelId: "model_fp16",
     quantization: "fp16",
     size: "163 MB",
   },
   {
-    model: "model_quantized",
+    modelId: "model_quantized",
     quantization: "8-bit",
     size: "92.4 MB",
   },
   {
-    model: "model_q8f16",
+    modelId: "model_q8f16",
     quantization: "Mixed precision",
     size: "86 MB",
   },
   {
-    model: "model_uint8",
+    modelId: "model_uint8",
     quantization: "8-bit & mixed precision",
     size: "177 MB",
   },
   {
-    model: "model_uint8f16",
+    modelId: "model_uint8f16",
     quantization: "Mixed precision",
     size: "114 MB",
   },
   {
-    model: "model_q4",
+    modelId: "model_q4",
     quantization: "4-bit matmul",
     size: "305 MB",
   },
   {
-    model: "model_q4f16",
+    modelId: "model_q4f16",
     quantization: "4-bit matmul & fp16 weights",
     size: "154 MB",
   },
@@ -48,10 +48,10 @@ export const modelsMap: Record<ModelId, Model> = Object.freeze(
   (() => {
     const map: Record<ModelId, Model> = {} as Record<ModelId, Model>;
     for (const model of models) {
-      map[model.model] = model;
+      map[model.modelId] = model;
     }
     return map;
   })(),
 );
 
-export const modelsIds = Object.freeze(models.map((model) => model.model));
+export const modelsIds = Object.freeze(models.map((model) => model.modelId));

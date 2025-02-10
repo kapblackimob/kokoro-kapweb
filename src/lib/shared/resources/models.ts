8fa38c88c90b844d99b3759aff3173d9af20f750
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-10 17:45:35 +0000

    Order models by size

--- src/lib/shared/resources/models.ts
+++ src/lib/shared/resources/models.ts
@@ -8,9 +8,29 @@ export const models = [
     size: "326 MB",
   },
   {
+    modelId: "model_q4",
+    quantization: "4-bit matmul",
+    size: "305 MB",
+  },
+  {
+    modelId: "model_uint8",
+    quantization: "8-bit & mixed precision",
+    size: "177 MB",
+  },
+  {
     modelId: "model_fp16",
     quantization: "fp16",
     size: "163 MB",
+  },
+  {
+    modelId: "model_q4f16",
+    quantization: "4-bit matmul & fp16 weights",
+    size: "154 MB",
+  },
+  {
+    modelId: "model_uint8f16",
+    quantization: "Mixed precision",
+    size: "114 MB",
   },
   {
     modelId: "model_quantized",
@@ -21,26 +41,6 @@ export const models = [
     modelId: "model_q8f16",
     quantization: "Mixed precision",
     size: "86 MB",
-  },
-  {
-    modelId: "model_uint8",
-    quantization: "8-bit & mixed precision",
-    size: "177 MB",
-  },
-  {
-    modelId: "model_uint8f16",
-    quantization: "Mixed precision",
-    size: "114 MB",
-  },
-  {
-    modelId: "model_q4",
-    quantization: "4-bit matmul",
-    size: "305 MB",
-  },
-  {
-    modelId: "model_q4f16",
-    quantization: "4-bit matmul & fp16 weights",
-    size: "154 MB",
   },
 ] as const;
 

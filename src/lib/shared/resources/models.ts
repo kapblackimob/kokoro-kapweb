f5899e2a51c5b3c02c289a36e941bbd05d15d994
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-10 16:33:06 +0000

    Refactor models structure to use array and improve type definitions; add modelsMap for easier access

--- src/lib/shared/resources/models.ts
+++ src/lib/shared/resources/models.ts
@@ -1,37 +1,57 @@
-export const models = Object.freeze({
-  model: {
+export type Model = (typeof models)[number];
+export type ModelId = Model["model"];
+
+export const models = Object.freeze([
+  {
+    model: "model",
     quantization: "fp32",
     size: "326 MB",
   },
-  model_fp16: {
+  {
+    model: "model_fp16",
     quantization: "fp16",
     size: "163 MB",
   },
-  model_quantized: {
+  {
+    model: "model_quantized",
     quantization: "8-bit",
     size: "92.4 MB",
   },
-  model_q8f16: {
+  {
+    model: "model_q8f16",
     quantization: "Mixed precision",
     size: "86 MB",
   },
-  model_uint8: {
+  {
+    model: "model_uint8",
     quantization: "8-bit & mixed precision",
     size: "177 MB",
   },
-  model_uint8f16: {
+  {
+    model: "model_uint8f16",
     quantization: "Mixed precision",
     size: "114 MB",
   },
-  model_q4: {
+  {
+    model: "model_q4",
     quantization: "4-bit matmul",
     size: "305 MB",
   },
-  model_q4f16: {
+  {
+    model: "model_q4f16",
     quantization: "4-bit matmul & fp16 weights",
     size: "154 MB",
   },
-});
+] as const);
 
-export type ModelId = keyof typeof models;
-export const modelIds: ModelId[] = Object.keys(models) as ModelId[];
+export const modelsMap: Record<ModelId, Model> = Object.freeze(
+  (() => {
+    const map: Record<ModelId, Model> = {} as Record<ModelId, Model>;
+    for (const model of models) {
+      map[model.model] = model;
+    }
+    return map;
+  })(),
+);
+
+export const modelsIds = Object.freeze(models.map((model) => model.model));

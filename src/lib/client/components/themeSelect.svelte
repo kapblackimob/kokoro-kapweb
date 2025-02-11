90c840ba9f0643b27ed67054c2e0bd0530730068
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-11 01:16:25 +0000

    Add theme selection component and integrate with layout

---
+++ src/lib/client/components/themeSelect.svelte
@@ -0,0 +1,11 @@
+<!-- This uses the following library to handle theme changes -->
+<!-- https://github.com/saadeghi/theme-change -->
+
+<label class="select">
+  <span class="label">Theme</span>
+  <select data-choose-theme>
+    <option value="">System 🖥️</option>
+    <option value="light">Light ☀️</option>
+    <option value="dracula">Dark 🌑</option>
+  </select>
+</label>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
   let { children } = $props();
   import "../app.css";
+  import ThemeSelect from "$lib/client/components/themeSelect.svelte";
 </script>
+
+<header>
+  <nav>
+    <ThemeSelect />
+  </nav>
+</header>
 
 {@render children()}

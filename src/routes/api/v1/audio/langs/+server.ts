d2961bb765ddf43c7cfc7f95cb7f5bd498ae9ed8
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-15 02:20:11 +0000

    Add API documentation for listing available languages

--- src/routes/api/v1/audio/langs/+server.ts
+++ src/routes/api/v1/audio/langs/+server.ts
@@ -2,6 +2,26 @@ import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import { langs } from "$lib/shared/resources";
 
+/**
+ * @api {get} /v1/audio/langs List available languages
+ * @apiName ListLanguages
+ * @apiGroup Speech
+ *
+ * @apiSuccess {Object[]} langs List of available languages
+ * @apiSuccess {String} langs.id Language ID
+ * @apiSuccess {String} langs.name Language name
+ *
+ * @apiSuccessExample {Object[]} Success-Response:
+ * HTTP/1.1 200 OK
+ * Content-Type: application/json
+ * [
+ *  {
+ *   "id": "en",
+ *   "name": "English",
+ *  }
+ * ]
+ */
+
 export const GET: RequestHandler = async () => {
   return json(langs);
 };

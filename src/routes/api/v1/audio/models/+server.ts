281af0f8442500e14324955b580f1ff008d84e09
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-15 02:23:54 +0000

    Add API documentation for listing available audio models

--- src/routes/api/v1/audio/models/+server.ts
+++ src/routes/api/v1/audio/models/+server.ts
@@ -2,6 +2,28 @@ import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import { models } from "$lib/shared/resources";
 
+/**
+ * @api {get} /v1/audio/models List available models
+ * @apiName ListModels
+ * @apiGroup Speech
+ *
+ * @apiSuccess {Object[]} models List of available models
+ * @apiSuccess {String} models.id Model ID
+ * @apiSuccess {String} models.quantization Model quantization type
+ * @apiSuccess {String} models.size Model size in MB
+ *
+ * @apiSuccessExample {Object[]} Success-Response:
+ * HTTP/1.1 200 OK
+ * Content-Type: application/json
+ * [
+ *  {
+ *   "id": "model",
+ *   "quantization": "fp32",
+ *   "size": "326 MB",
+ *  }
+ * ]
+ */
+
 export const GET: RequestHandler = async () => {
   return json(models);
 };

8d55e75ab77bdeb14a7b73aa98e9e7f5440e5eed
Luis Eduardo <eduardo.devop@gmail.com>
2025-02-15 02:32:01 +0000

    Add API documentation for listing available voices

--- src/routes/api/v1/audio/voices/+server.ts
+++ src/routes/api/v1/audio/voices/+server.ts
@@ -2,6 +2,37 @@ import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import { voices } from "$lib/shared/resources";
 
+/**
+ * @api {get} /v1/audio/voices List available voices
+ * @apiName ListVoices
+ * @apiGroup Speech
+ *
+ * @apiSuccess {Object[]} voices List of available voices
+ * @apiSuccess {String} voices.id Voice ID
+ * @apiSuccess {String} voices.name Voice name
+ * @apiSuccess {String} voices.gender Voice gender
+ * @apiSuccess {String} voices.targetQuality Voice target quality
+ * @apiSuccess {String} voices.overallGrade Voice overall grade
+ * @apiSuccess {Object} voices.lang Voice language
+ * @apiSuccess {String} voices.lang.id Language ID
+ * @apiSuccess {String} voices.lang.name Language name
+ *
+ * @apiSuccessExample {Object[]} Success-Response:
+ *   [
+ *     {
+ *       "id": "af_heart",
+ *       "name": "Heart",
+ *       "gender": "Female",
+ *       "targetQuality": "A",
+ *       "overallGrade": "A",
+ *       "lang": {
+ *         "id": "en-us",
+ *         "name": "English (US)"
+ *       }
+ *     }
+ *   ]
+ */
+
 export const GET: RequestHandler = async () => {
   return json(voices);
 };
